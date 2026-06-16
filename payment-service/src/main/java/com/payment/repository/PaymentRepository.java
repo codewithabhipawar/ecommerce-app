1d31c5cbbd34a2050416e9c25ebdc6264c815009
abhi_pawar <ap7218362896@gmail.com>
2026-06-16 11:02:27 +0530

    payment-service repository manage

---
+++ payment-service/src/main/java/com/payment/repository/PaymentRepository.java
@@ -0,0 +1,16 @@
+package com.payment.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.payment.entity.Payment;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, String>{
+	
+	List<Payment> findByUserId(String userId);
+	Payment findByOrderId(int orderId);
+
+}
