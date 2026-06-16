d02bf1b8238b299649987ae6ca715e083f5a3264
abhi_pawar <ap7218362896@gmail.com>
2026-06-16 11:02:55 +0530

    payment-service service provided

---
+++ payment-service/src/main/java/com/payment/service/PaymentService.java
@@ -0,0 +1,44 @@
+package com.payment.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.payment.entity.Payment;
+import com.payment.exception.PaymentNotFoundException;
+import com.payment.repository.PaymentRepository;
+
+@Service
+public class PaymentService {
+
+	@Autowired
+	private PaymentRepository paymentRepository;
+
+	// get All payments
+	public List<Payment> getAllPayments() {
+		return paymentRepository.findAll();
+	}
+
+	// get single payment by paymentId
+	public Payment getSinglePayment(String paymentId) throws PaymentNotFoundException {
+		return paymentRepository.findById(paymentId)
+				.orElseThrow(() -> new PaymentNotFoundException("payment not found for this payment id: " + paymentId));
+	}
+
+	// get payment by order id
+	public Payment getPaymentByOrderId(int orderId) {
+		return paymentRepository.findByOrderId(orderId);
+	}
+
+	// get payment by user id
+	public List<Payment> getPaymentByUserId(String userId) {
+		return paymentRepository.findByUserId(userId);
+	}
+
+	// create payment
+	public Payment createPayment(Payment payment) {
+		return paymentRepository.save(payment);
+	}
+
+}
