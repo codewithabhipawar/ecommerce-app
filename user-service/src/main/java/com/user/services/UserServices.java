97a47a13e4e48d67db1a562219f513abc16c5d9e
abhi_pawar <ap7218362896@gmail.com>
2026-06-17 18:30:37 +0530

    user-service modify

--- user-service/src/main/java/com/user/services/UserServices.java
+++ user-service/src/main/java/com/user/services/UserServices.java
@@ -1,13 +1,18 @@
 package com.user.services;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.user.entity.Product;
 import com.user.entity.User;
 import com.user.exceptions.ResourceNotFoundException;
+import com.user.externalservices.ProductService;
 import com.user.repository.UserRepository;
 
 @Service
@@ -15,6 +20,12 @@ public class UserServices {
 	
 	@Autowired
 	private UserRepository userRepo;
+	
+	@Autowired
+	private ProductService productService;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	public List<User> getAllUser()
 	{
@@ -30,7 +41,18 @@ public class UserServices {
 	
 	public User getById(String userId) throws ResourceNotFoundException
 	{
-		return userRepo.findById(userId).orElseThrow(() -> new ResourceNotFoundException());
+		User user = userRepo.findById(userId).orElseThrow(() -> new ResourceNotFoundException());
+		//
+		
+//		Order order = restTemplate.getForObject("", Order.class);
+//		user.setOrders(order);
+//		Product[] products = restTemplate.getForObject("http://localhost:8082/product/users/"+user.getUserId(), Product[].class);
+//		List<Product> collect = Arrays.stream(products).collect(Collectors.toList());
+//		user.setProducts(collect);
+		
+		List<Product> products = productService.getProducts(userId);
+		user.setProducts(products);
+		return user;
 	}
 	
 	public User updateUser(User user)
