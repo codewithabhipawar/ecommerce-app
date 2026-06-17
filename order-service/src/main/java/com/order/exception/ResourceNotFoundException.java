3e743ae602f3e4bce8d674d881002376dc86ed6a
abhi_pawar <ap7218362896@gmail.com>
2026-06-17 18:35:19 +0530

    order-service exception handled

---
+++ order-service/src/main/java/com/order/exception/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package com.order.exception;
+
+@SuppressWarnings("serial")
+public class ResourceNotFoundException extends RuntimeException {
+	
+	public ResourceNotFoundException(String message)
+	{
+		super(message);
+	}
+	
+	public ResourceNotFoundException()
+	{
+		super("resource not found");
+	}
+
+}
