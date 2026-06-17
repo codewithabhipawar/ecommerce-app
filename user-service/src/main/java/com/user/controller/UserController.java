dfdb73084fe66ed32b885127cf7fa9faadad0671
abhi_pawar <ap7218362896@gmail.com>
2026-06-17 18:28:02 +0530

    user-service user controller modify

--- user-service/src/main/java/com/user/controller/UserController.java
+++ user-service/src/main/java/com/user/controller/UserController.java
@@ -11,42 +11,44 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.user.entity.User;
 import com.user.services.UserServices;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 	
 	@Autowired
 	private UserServices service;
 	
-	@GetMapping("/users")
+	@GetMapping
 	public ResponseEntity<List<User>> getAllUsers()
 	{
 		return ResponseEntity.ok(service.getAllUser());
 	}
 	
-	@GetMapping("/user/{userId}")
+	@GetMapping("/{userId}")
 	public ResponseEntity<User> usersGetById(@PathVariable("userId") String userId)
 	{
 		return ResponseEntity.ok(service.getById(userId));
 	}
 	
-	@PostMapping("/user")
+	@PostMapping
 	public ResponseEntity<User> saveUser(@RequestBody User user)
 	{
 		return ResponseEntity.status(HttpStatus.CREATED).body(service.createUser(user));
 	}
 	
-	@PutMapping("/user/update")
+	@PutMapping("/update")
 	public ResponseEntity<User> userUpdate(User user)
 	{
 		return ResponseEntity.status(HttpStatus.OK).body(service.updateUser(user));
 	}
 	
-	@DeleteMapping("/user/{userId}")
+	@DeleteMapping("/{userId}")
 	public ResponseEntity<User> deleteUser(@PathVariable("userId") String userId)
 	{
 		return ResponseEntity.status(HttpStatus.FOUND).body(service.deleteUser(userId));
