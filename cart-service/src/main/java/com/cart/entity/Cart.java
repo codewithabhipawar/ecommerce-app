8ee7fa5963cf74733249ed63bdc9ffbdafb765c1
abhi_pawar <ap7218362896@gmail.com>
2026-06-18 22:05:10 +0530

    cart-service cart class modify

--- cart-service/src/main/java/com/cart/entity/Cart.java
+++ cart-service/src/main/java/com/cart/entity/Cart.java
@@ -1,18 +1,19 @@
 package com.cart.entity;
 
 
+
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
@@ -20,16 +21,18 @@ import lombok.Setter;
 @NoArgsConstructor
 @Builder
 @Entity
+@ToString
+@Table(name = "carts")
 public class Cart {
 	
 	@Id
 	private String cartId;
 	private String userId;
 	private String status;
-	
-	@OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
-	private List<Item> items;
 	private double shipping_amount;
 	private double total_amount;
+	
+	@Transient
+	private List<Product> products;
 
 }
