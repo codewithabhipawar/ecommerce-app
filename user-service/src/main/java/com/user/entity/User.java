7a18190000c031d548b6c4788237c98a3e70958a
abhi_pawar <ap7218362896@gmail.com>
2026-06-18 21:52:35 +0530

    user-service user class modify

--- user-service/src/main/java/com/user/entity/User.java
+++ user-service/src/main/java/com/user/entity/User.java
@@ -1,8 +1,8 @@
 package com.user.entity;
 
-import java.util.List;
 
-import org.hibernate.annotations.Table;
+
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -32,12 +32,9 @@ public class User {
 	private String about;
 	
 	@Transient
-	private List<Product> products;
-	
-	@Transient
 	private Cart cart;
 	
 	@Transient
-	private Order orders;
+	private List<Order> orders;
 
 }
